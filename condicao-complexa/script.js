d3047bbc2a3ae8b5b8534078f061a6a9631cfca5
liviamazutti <liviamazutti7@gmail.com>
2026-03-18 13:19:31 -0300

    meu primeiro comit

---
+++ condicao-complexa/script.js
@@ -0,0 +1,8 @@
+let idade = Number (prompt("insira sua idade:"));
+let temCNH= prompt ("possui CNH? (sim ou nao)");
+
+if (idade >= 18 && temCNH === "sim")  {
+    alert("Você pode dirigir");
+} else {
+    alert("você nso pode dirigir");
+}
